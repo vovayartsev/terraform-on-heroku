fa5df4ada1c6202595421b791f6e4485e208ce5d
Vladimir Yartsev <vovayartsev@gmail.com>
2018-02-07 01:44:45 +0400

    AWS_CONTAINER_CREDENTIALS_RELATIVE_URI

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 console.log("Hello from Fargate")
 console.log(process.env)
+
+const got = require('got');
+
+got(`http://169.254.170.2${process.env.AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}`).then(function(response){
+  console.log(response.body)
+})
+
+setTimeout(function() {console.log("Done")}, 10000)
